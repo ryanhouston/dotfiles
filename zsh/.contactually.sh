21312a5abdad662876bfd945f3ea414f0e4871b1
Ryan Houston <ryanhouston83@gmail.com>
2017-09-13 20:47:09 -0400

    Update contactually aliases

--- zsh/.contactually.sh
+++ zsh/.contactually.sh
@@ -1,15 +1,3 @@
-ctly-staging() {
-  heroku accounts:set contactually
-  echo "heroku $@ --app contactually-staging"
-  heroku $@ --app contactually-staging;
-}
-
-ctly-prod() {
-  heroku accounts:set contactually
-  echo "heroku $@ --app contactually"
-  heroku $@ --app contactually;
-}
-
 deploy.ctly-staging() {
   heroku accounts:set contactually
   bundle exec rake ctly:deploy;
